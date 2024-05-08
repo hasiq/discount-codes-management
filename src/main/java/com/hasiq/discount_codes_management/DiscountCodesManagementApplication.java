3a4ff3509e5831b9dafed3bfec93a93c15cfd14a
hasiq <jhasikowski@gmail.com>
2024-05-08 10:51:24 +0200

    initial commit

---
+++ src/main/java/com/hasiq/discount_codes_management/DiscountCodesManagementApplication.java
@@ -0,0 +1,13 @@
+package com.hasiq.discount_codes_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountCodesManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscountCodesManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hasiq/discount_codes_management/DiscountCodesManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasiq.discount_codes_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscountCodesManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
